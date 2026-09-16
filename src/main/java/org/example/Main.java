010890c55ddaa054bb2d62242854413e6789f3c2
SandeepKottala <sandeepreddykottala@gmail.com>
2026-09-16 13:37:21 +0530

    Integrated PromptTemplate with Messages Types

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,54 +1,60 @@
 package org.example;
 
 
-import dev.langchain4j.data.message.AiMessage;
-import dev.langchain4j.data.message.ChatMessage;
-import dev.langchain4j.data.message.SystemMessage;
-import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.data.message.*;
 import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.chat.response.ChatResponse;
 import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
-
+import dev.langchain4j.model.input.Prompt;
+import dev.langchain4j.model.input.PromptTemplate;
 
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static com.fasterxml.jackson.databind.type.LogicalType.Map;
+
 
 class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hi");
-
         String gemini_api_key = System.getenv("GEMINI_API_KEY");
 
         //Creating chat model
-
         ChatModel model = GoogleAiGeminiChatModel.builder()
                 .apiKey(gemini_api_key)
                 .modelName("gemini-3.6-flash")
                 .build();
 
-
         //System Message
-
         SystemMessage systemMessage = SystemMessage.from(
-                "You are an expert Java teacher" +
-                        "Explain Java concepts in simple language" +
-                        "with easy examples.");
-        //User Message
-
-        UserMessage userMessage = UserMessage.from(
-                "What is polymorphism in Java?"
+                """
+                       You are an expert Java teacher
+                       Explain Java concepts in simple language
+                       with easy examples
+                     """
         );
+
+        //Prompt Template
+
+        PromptTemplate promptTemplate = PromptTemplate.from(
+                "What is {{topic}} in Java?"
+        );
+
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("topic", "Polymorphism");
+
+        Prompt prompt = promptTemplate.apply(variables);
+
+        System.out.println("THE PROMPT IS : " + prompt);
+
+        //User Message
+        UserMessage userMessage = UserMessage.from(prompt.text());
 
         List<ChatMessage> messages = List.of(
                 systemMessage,
                 userMessage
-
         );
 
         ChatResponse response = model.chat(messages);
